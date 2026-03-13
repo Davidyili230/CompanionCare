5d045b1c182d72a42f0a43adddbd7fcb1cb95f1a
David <YI.LI85@myhunter.cuny.edu>
2026-03-13 00:25:02 -0400

    updated UX

---
+++ src/pages/Missing/Missing.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function Missing() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#FFF9F0", boxSizing: "border-box" }}>
+      <Navbar />
+    </div>
+  );
+}
