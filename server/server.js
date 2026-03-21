d023a4cab192af1e6011595f0513a0b51a63e35b
Jin Guo <gj1609793251@gmail.com>
2026-03-21 17:34:17 -0400

    pet photo fix + supplement suggestion (no real API key yet)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import path from "path"
 import communityRoutes from "./routes/community.routes.js"
 import uploadRoutes from "./routes/upload.routes.js"
+import ebayRecommendationsRoute from "./routes/ebayRecommendations.js";
 
 const app = express();
 app.use(express.json()); //allow back to accept json type data // Ruifeng
@@ -22,6 +23,7 @@ const Upload_Dir = path.join(process.cwd(), "uploads")  //current working direct
 app.use("/uploads", express.static(Upload_Dir))
 app.use("/api/community", communityRoutes)
 app.use("/api/uploads", uploadRoutes)
+app.use("/api/ebay-recommendations", ebayRecommendationsRoute);
 
 
 const PORT = 8080;
