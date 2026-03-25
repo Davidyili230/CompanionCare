38a6e33fdc8ae372071fa32b4bb65c1de133cc48
Anthony <zhangruifeng@s-MacBook-Pro.lan>
2026-03-25 03:20:36 -0400

    some change

--- src/api/community.api.js
+++ src/api/community.api.js
@@ -1,7 +1,53 @@
 export async function fetchCommunityPosts() {
-  const res = await fetch("http://192.168.1.136:8080/api/community/posts")
-  if(!res.ok){
-    throw new Error("Failed to fetch posts")
+   const res = await fetch("/api/community/posts");
+   if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+   }
+   return res.json(); //frontend is called data
+}
+
+//for Cloudinary
+async function uploadToCloudinary(file) {
+   const formData = new FormData();
+   formData.append("file", file);
+   formData.append("upload_preset", "CompanionCare");
+   //upload_preset is i setup in cloudinary, is called unsigned upload
+
+   const resourceType = file.type.startsWith("video/") ? "video" : "image";
+
+   const res = await fetch(
+      `https://api.cloudinary.com/v1_1/dap1qzjmi/${resourceType}/upload`,
+      //dap1qzjmi is the name in cloudnary
+      { method: "POST", body: formData },
+   );
+
+   if (!res.ok) throw new Error("failed to upload the media");
+   const data = await res.json();
+   return { type: resourceType, url: data.secure_url };
+}
+
+export async function createCommunityPost({ title, content, label, images = [], video = null }) {
+  // upload media in Cloudinary
+  const mediaUrls = [];
+
+  for (let i = 0; i < images.length; i++) {
+    const result = await uploadToCloudinary(images[i]);
+    mediaUrls.push(result);
+    //mediaurls is {type:"", url:""}
   }
-  return res.json()   //frontend is called data
+
+  if (video) {
+    const result = await uploadToCloudinary(video);
+    mediaUrls.push(result);
+  }
+
+  // type and url, POST to backend
+  const res = await fetch("/api/community/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, content, label, media: mediaUrls }),
+  });
+
+  if (!res.ok) throw new Error("Failed to post");
+  return res.json();
 }
