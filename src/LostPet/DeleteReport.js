d6ed0da0d96f935725ff5259be669f862d3125dd
Kelvin Chen <kelvin@macbookpro.mynetworksettings.com>
2026-03-25 16:15:51 -0400

    attempt to fix image deletion from storage

--- src/LostPet/DeleteReport.js
+++ src/LostPet/DeleteReport.js
@@ -2,14 +2,19 @@
 
 import { db } from "../firebase/firebase";
 import { deleteDoc, doc} from "firebase/firestore";
+import { getStorage, ref, deleteObject } from "firebase/storage";
 
 
-export async function deleteReport(id) {
+export async function deleteReport(id, imageUrl) {
     try {
         const report = doc(db, "lostPetReports", id);
         await deleteDoc(report)
+
+        const imageRef = ref(storage, imageUrl);
+        await deleteObject(imageRef)
+
         console.log("Report was deleted");
     } catch (error) {
-        console.log("Failed to delte a report", error);
+        console.log("Failed to delete a report", error);
     }
 }
