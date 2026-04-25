5ede66cce4e66e08f2dabe25e350bd8a5ad3458e
Jin Guo <gj1609793251@gmail.com>
2026-04-24 20:35:46 -0400

    DashBoard UI/UX Optimization + Bug fix

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,7 +1,7 @@
 export default function PetCard({ pet, selected, onClick, onDelete }) {
   return (
     <div
-      className={`flex w-full min-h-[230px] flex-col items-center rounded-[22px] border bg-white px-4 py-4 text-center transition-all duration-200 ${
+      className={`flex w-full min-h-57.5 flex-col items-center rounded-[22px] border bg-white px-4 py-4 text-center transition-all duration-200 ${
         selected
           ? "border-[#de7e52] bg-[#fcf5ef]"
           : "border-[#de7e52] hover:bg-[#fcf5ef]"
@@ -12,7 +12,7 @@ export default function PetCard({ pet, selected, onClick, onDelete }) {
         onClick={() => onClick(pet.id)}
         className="flex w-full flex-1 flex-col items-center text-center pb-3"
       >
-        <div className="mb-3 flex h-[74px] w-[74px] items-center justify-center overflow-hidden rounded-full border border-[#9a9a9a] bg-[#f2f2f2]">
+        <div className="mb-3 flex h-18.5 w-18.5 items-center justify-center overflow-hidden rounded-full border border-[#9a9a9a] bg-[#f2f2f2]">
           {pet.image ? (
             <img
               src={pet.image}
